1a9c4bcf42d26b397273b3db4b6be02763ff44de
eselbeus <eselbeus@gmail.com>
2019-02-09 18:41:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,15 +1,16 @@
 class Show < ActiveRecord::Base 
   def self.highest_rating 
-    shows_arr = Show.all.map do |show|
-      show.rating
-    end
-    shows_arr.sort[-1]
+    Show.all.rating.maximum
+    # shows_arr = Show.all.map do |show|
+    #   show.rating
+    # end
+    # shows_arr.sort[-1]
   end
-  # def most_popular_show 
-  #   Shows.all.each do |show|
-  #     if show.ratings == highest_rating
-  #       return show
-  #     end
-  #   end
-  # end
+  def most_popular_show 
+    Shows.all.each do |show|
+      if show.ratings == highest_rating
+        return show
+      end
+    end
+  end
 end
