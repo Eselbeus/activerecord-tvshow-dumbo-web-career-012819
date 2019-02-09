86067296ab57726c81954a84ec2f930f6595d06f
eselbeus <eselbeus@gmail.com>
2019-02-09 18:27:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,8 @@
 class Show < ActiveRecord::Base 
   def highest_rating 
-    Shows.all.ratings.maximum
+    Shows.all.map do |show|
+      show.ratings
+    end.maximum
   end
   # def most_popular_show 
   #   Shows.all.each do |show|
