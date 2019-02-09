53f481ec0b40d4ea28082ff828d7c028b0b51842
eselbeus <eselbeus@gmail.com>
2019-02-09 18:26:17 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,6 @@
-require 'pry'
-
 class Show < ActiveRecord::Base 
   def highest_rating 
     Shows.all.ratings.maximum
-    binding.pry
   end
   # def most_popular_show 
   #   Shows.all.each do |show|
